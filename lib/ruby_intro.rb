e5b0b18ecf31d0a6b031108d142ecee73321f434
Jason Wangsadinata <jason.christian19@gmail.com>
2016-02-01 05:08:10 +0000

    Completed hw0

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -26,25 +26,48 @@ def max_2_sum arr
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  sortedArr = arr.sort
+  for i in 0..sortedArr.length - 1
+    for j in (i+1)..sortedArr.length - 1
+      if (sortedArr[i] + sortedArr[j] == n)
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name.to_s
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return s =~ /\A[bBcCdDfFgGhHjJkKlLmMnNpPqQrRsStTvVwWxXyYzZ]/
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return s.to_i(2) % 4 == 0 && s =~ /^[0-1]+$/i
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price)
+    if isbn == "" || price <= 0
+      raise ArgumentError
+    else
+      @isbn = isbn
+      @price = price
+    end
+  end
+  
+  def price_as_string
+    decimal = (price * 100).to_i % 100
+    return (decimal < 10) ? "$" + price.to_i.to_s + ".0" + decimal.to_s : "$" + price.to_i.to_s + "." + decimal.to_s
+    
+  end
 end
